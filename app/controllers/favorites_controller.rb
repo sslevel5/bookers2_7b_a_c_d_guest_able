758549703fb89020013420f52d3e9879d4b6e5fc
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-02-15 12:24:14 +0000

    いいね非同期通信化_応用課題6_20240215

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,7 @@ class FavoritesController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
 
@@ -9,6 +10,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
 
