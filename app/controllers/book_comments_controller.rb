0324e3d06abd02525dd63cac05ee7e9a0a2681d7
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-02-16 08:41:33 +0000

    削除機能非同期通信化_応用課題6_20240216

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -13,8 +13,9 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-     comment = BookComment.find(params[:id])
-     comment.destroy
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
 
