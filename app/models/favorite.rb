ac8c789adbcf1802e968c5f9c41492ee9136b47c
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-02-19 00:27:13 +0000

    いいね数_応用課題7a_20240219

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,15 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  
+
   validates :user_id, uniqueness: {scope: :book_id}
+
+  def get_book_week_favorite_count
+    favorite_week_start = Date.today.beginning_of_day - 6.days
+    favorite_week_end = Date.today.end_of_day
+
+    self.favorites.where(created_at: favorite_week_start..favorite_week_end).count
+  end
+
+
 end
