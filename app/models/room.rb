d7039f7234302afac13bcd3ef63a3f563ab47f4e
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-02-20 15:11:37 +0000

    e_DM_20240220

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
 
-  has_many :messages, dependent: :destroy
+  has_many :messages, -> { order(created_at: :desc) }, dependent: :destroy
   has_many :entries, dependent: :destroy
 
 end
