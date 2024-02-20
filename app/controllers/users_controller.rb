957ce54a4aaa756019f8357460016d7e4071816b
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-02-20 14:02:34 +0000

    view_20240220

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,20 +8,22 @@ class UsersController < ApplicationController
     @book = Book.new
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
-    else
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
+    if current_user
+      if @user.id == current_user.id
+      else
+        @currentUserEntry.each do |cu|
+          @userEntry.each do |u|
+            if cu.room_id == u.room_id then
+              @isRoom = true
+              @roomId = cu.room_id
+            end
           end
         end
-      end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
+        if @isRoom
+        else
+          @room = Room.new
+          @entry = Entry.new
+        end
       end
     end
   end
