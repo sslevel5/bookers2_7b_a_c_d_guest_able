2bee53edf45b7e5bb9c2e14f5b0b99919eaeb5b0
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-02-20 10:13:34 +0000

    ルーティング_20240220

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "search" => "searches#search"
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
 
 end
