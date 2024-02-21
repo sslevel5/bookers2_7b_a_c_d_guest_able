3f31bfc90b61ec649fa0540948cc18c4cf208414
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-02-21 09:28:33 +0000

    閲覧数表示_20240221

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,8 @@ class BooksController < ApplicationController
     @user = @book.user
     @book_comment = BookComment.new
     @book_detail = Book.find(params[:id])
-    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
-      current_user.view_counts.create(book_id: @book_detail.id)
+    unless ReadCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.read_counts.create(book_id: @book_detail.id)
     end
   end
 
