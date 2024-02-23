638668812621849d5e45bf378f29df75087d52d3
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-02-23 01:44:00 +0000

    準備_20240223

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,18 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port:                 587,
-    address:              'smtp.gmail.com',
-    domain:               'gmail.com',
-    user_name:            'sslevel5dmw@gmail.com',
-    password:             'dklyayvfcnjxuowh',
-    authentication:       'login',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -85,4 +74,18 @@ Rails.application.configure do
   config.hosts.clear
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  port:                 587,
+  address:              'smtp.gmail.com',
+  domain:               'gmail.com',
+  user_name:            'sslevel5dmw@gmail.com',
+  password:             'dklyayvfcnjxuowh',
+  authentication:       'login',
+  enable_starttls_auto: true
+  }
+
+
 end
