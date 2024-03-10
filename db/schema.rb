050cb6c2d21fa8edce0d5d4fc9e269c94d935a0a
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-10 10:43:10 +0000

    rails db:migrate_20240310

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_25_052934) do
+ActiveRecord::Schema.define(version: 2024_03_10_104044) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -102,6 +102,17 @@ ActiveRecord::Schema.define(version: 2024_02_25_052934) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "notifiable_type", null: false
+    t.integer "notifiable_id", null: false
+    t.boolean "read", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "read_counts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "book_id", null: false
@@ -147,6 +158,7 @@ ActiveRecord::Schema.define(version: 2024_02_25_052934) do
   add_foreign_key "group_users", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "notifications", "users"
   add_foreign_key "read_counts", "books"
   add_foreign_key "read_counts", "users"
   add_foreign_key "rooms", "users"
